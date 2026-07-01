2c2a10d643331eb02dbca4ef6b0ce7d2c72a3ce0
Awais Mahaar <awaismahaar499@gmail.com>
2026-07-01 15:42:52 +0500

    add isProduction true

--- server/src/common/filters/http-exception.filter.ts
+++ server/src/common/filters/http-exception.filter.ts
@@ -19,7 +19,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
-    const isProduction = process.env.NODE_ENV === 'production';
+    const isProduction = true;
 
     let statusCode: number = HttpStatus.INTERNAL_SERVER_ERROR;
     let message: string | string[] = 'Internal server error';
